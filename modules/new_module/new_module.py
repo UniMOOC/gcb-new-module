e30d433153d9ad2f1d931f7f48b0ebbe9ba6763c
Martín Candela Calabuig <rellikiox@gmail.com>
2015-04-15 12:52:01 +0200

    Add enable and dissable hooks

--- modules/new_module/new_module.py
+++ modules/new_module/new_module.py
@@ -1,4 +1,6 @@
 
+import logging
+
 from models import custom_modules
 
 from . import handlers
@@ -6,6 +8,12 @@ from . import handlers
 
 def register_module():
     """Registers this module in the registry."""
+
+    def on_module_enabled():
+        logging.info('Module new_module.py was just enabled')
+
+    def on_module_disabled():
+        logging.info('Module new_module.py was just dissabled')
 
     global_urls = [
         ('/new-global-url', handlers.NewURLHandler)  # Global URLs go on mycourse.appspot.com/url
@@ -18,5 +26,7 @@ def register_module():
     custom_module = custom_modules.Module(
         'New module title (has to be unique)',
         'Implements some functionality',
-        global_urls, course_urls)
+        global_urls, course_urls,
+        notify_module_disabled=on_module_disabled,
+        notify_module_enabled=on_module_enabled)
     return custom_module
